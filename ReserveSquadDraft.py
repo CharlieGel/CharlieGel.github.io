6046bf0f1272e975cd0fe6bf719c9f3f329956f3
Charlie Gelman <charliegelman@Charlies-MacBook-Pro.local>
2019-08-09 21:04:10 -0400

    Ready for the draft!

---
+++ ReserveSquadDraft.py
@@ -0,0 +1,552 @@
+if __name__ == '__main__':
+    text_2018 = """Round 1
+    NO.
+    PLAYER
+    TEAM
+    1
+    David Johnson Ari, RB
+    Cyka Blyat
+    2
+    Le'Veon Bell Pit, RB
+    Team Brannigan
+    3
+    Antonio Brown Pit, WR
+    #FreeZeke Krispy Kareem
+    4
+    Julio Jones Atl, WR
+    CigsInside SMOKINJAYCUTLER
+    5
+    Odell Beckham Jr. NYG, WR
+    Blowjobs Dont Take Virginity
+    6
+    A.J. Green Cin, WR
+    Team TradeRapist
+    7
+    LeSean McCoy Buf, RB
+    Josh Thordon
+    8
+    Jordy Nelson Oak, WR
+    Kissing TDs
+    9
+    Melvin Gordon LAC, RB
+    Paige Still Has Syphilis
+    10
+    Mike Evans TB, WR
+    Team Harzich
+    Round 2
+    NO.
+    PLAYER
+    TEAM
+    1
+    Devonta Freeman Atl, RB
+    Team Harzich
+    2
+    Jordan Howard Chi, RB
+    Paige Still Has Syphilis
+    3
+    Rob Gronkowski NE, TE
+    Kissing TDs
+    4
+    Doug Baldwin Sea, WR
+    Josh Thordon
+    5
+    Jay Ajayi Phi, RB
+    Team TradeRapist
+    6
+    DeMarco Murray Ten, RB
+    Blowjobs Dont Take Virginity
+    7
+    Leonard Fournette Jax, RB
+    CigsInside SMOKINJAYCUTLER
+    8
+    Ezekiel Elliott Dal, RB
+    #FreeZeke Krispy Kareem
+    9
+    Demaryius Thomas Den, WR
+    Team Brannigan
+    10
+    Michael Thomas NO, WR
+    Cyka Blyat
+    Round 3
+    NO.
+    PLAYER
+    TEAM
+    1
+    Dez Bryant Dal, WR
+    Cyka Blyat
+    2
+    Lamar Miller Hou, RB
+    Team Brannigan
+    3
+    T.Y. Hilton Ind, WR
+    #FreeZeke Krispy Kareem
+    4
+    Amari Cooper Oak, WR
+    CigsInside SMOKINJAYCUTLER
+    5
+    Aaron Rodgers GB, QB
+    Blowjobs Dont Take Virginity
+    6
+    Todd Gurley II LAR, RB
+    Team TradeRapist
+    7
+    Alshon Jeffery Phi, WR
+    Josh Thordon
+    8
+    DeAndre Hopkins Hou, WR
+    Kissing TDs
+    9
+    Brandin Cooks LAR, WR
+    Paige Still Has Syphilis
+    10
+    Marshawn Lynch Oak, RB
+    Team Harzich
+    Round 4
+    NO.
+    PLAYER
+    TEAM
+    1
+    Terrelle Pryor Sr. NYJ, WR
+    Team Harzich
+    2
+    Christian McCaffrey Car, RB
+    Paige Still Has Syphilis
+    3
+    Tom Brady NE, QB
+    Kissing TDs
+    4
+    Carlos Hyde Cle, RB
+    Josh Thordon
+    5
+    Isaiah Crowell NYJ, RB
+    Team TradeRapist
+    6
+    LeGarrette Blount Det, RB
+    Blowjobs Dont Take Virginity
+    7
+    Julian Edelman NE, WR
+    CigsInside SMOKINJAYCUTLER
+    8
+    Ty Montgomery GB, RB
+    #FreeZeke Krispy Kareem
+    9
+    Michael Crabtree Bal, WR
+    Team Brannigan
+    10
+    Joe Mixon Cin, RB
+    Cyka Blyat
+    Round 5
+    NO.
+    PLAYER
+    TEAM
+    1
+    Matt Ryan Atl, QB
+    Cyka Blyat
+    2
+    Drew Brees NO, QB
+    Team Brannigan
+    3
+    Keenan Allen LAC, WR
+    #FreeZeke Krispy Kareem
+    4
+    Travis Kelce KC, TE
+    CigsInside SMOKINJAYCUTLER
+    5
+    Frank Gore Mia, RB
+    Blowjobs Dont Take Virginity
+    6
+    Greg Olsen Car, TE
+    Team TradeRapist
+    7
+    Jordan Reed Wsh, TE
+    Josh Thordon
+    8
+    Golden Tate Det, WR
+    Kissing TDs
+    9
+    Larry Fitzgerald Ari, WR
+    Paige Still Has Syphilis
+    10
+    Jarvis Landry Cle, WR
+    Team Harzich
+    Round 6
+    NO.
+    PLAYER
+    TEAM
+    1
+    Allen Robinson Chi, WR
+    Team Harzich
+    2
+    Tyreek Hill KC, WR
+    Paige Still Has Syphilis
+    3
+    Danny Woodhead Bal, RB
+    Kissing TDs
+    4
+    Emmanuel Sanders Den, WR
+    Josh Thordon
+    5
+    Kelvin Benjamin Buf, WR
+    Team TradeRapist
+    6
+    Stefon Diggs Min, WR
+    Blowjobs Dont Take Virginity
+    7
+    Dalvin Cook Min, RB
+    CigsInside SMOKINJAYCUTLER
+    8
+    Spencer Ware KC, RB
+    #FreeZeke Krispy Kareem
+    9
+    Eddie Lacy Sea, RB
+    Team Brannigan
+    10
+    Mark Ingram NO, RB
+    Cyka Blyat
+    Round 7
+    NO.
+    PLAYER
+    TEAM
+    1
+    Kyle Rudolph Min, TE
+    Cyka Blyat
+    2
+    Davante Adams GB, WR
+    Team Brannigan
+    3
+    Darren McFadden Dal, RB
+    #FreeZeke Krispy Kareem
+    4
+    Matthew Stafford Det, QB
+    CigsInside SMOKINJAYCUTLER
+    5
+    Delanie Walker Ten, TE
+    Blowjobs Dont Take Virginity
+    6
+    Andrew Luck Ind, QB
+    Team TradeRapist
+    7
+    Russell Wilson Sea, QB
+    Josh Thordon
+    8
+    Bilal Powell NYJ, RB
+    Kissing TDs
+    9
+    Jamison Crowder Wsh, WR
+    Paige Still Has Syphilis
+    10
+    Sammy Watkins KC, WR
+    Team Harzich
+    Round 8
+    NO.
+    PLAYER
+    TEAM
+    1
+    Pierre Garcon SF, WR
+    Team Harzich
+    2
+    Jimmy Graham GB, TE
+    Paige Still Has Syphilis
+    3
+    Willie Snead NO, WR
+    Kissing TDs
+    4
+    Brandon Marshall NYG, WR
+    Josh Thordon
+    5
+    Paul Perkins NYG, RB
+    Team TradeRapist
+    6
+    Adrian Peterson Ari, RB
+    Blowjobs Dont Take Virginity
+    7
+    Tyler Eifert Cin, TE
+    CigsInside SMOKINJAYCUTLER
+    8
+    Ameer Abdullah Det, RB
+    #FreeZeke Krispy Kareem
+    9
+    Zach Ertz Phi, TE
+    Team Brannigan
+    10
+    Donte Moncrief Jax, WR
+    Cyka Blyat
+    Round 9
+    NO.
+    PLAYER
+    TEAM
+    1
+    Mike Gillislee NE, RB
+    Cyka Blyat
+    2
+    Matt Forte NYJ, RB
+    Team Brannigan
+    3
+    Martavis Bryant Pit, WR
+    #FreeZeke Krispy Kareem
+    4
+    Jeremy Maclin Bal, WR
+    CigsInside SMOKINJAYCUTLER
+    5
+    Doug Martin Oak, RB
+    Blowjobs Dont Take Virginity
+    6
+    Kenny Britt NE, WR
+    Team TradeRapist
+    7
+    Eric Decker Ten, WR
+    Josh Thordon
+    8
+    Theo Riddick Det, RB
+    Kissing TDs
+    9
+    Derek Carr Oak, QB
+    Paige Still Has Syphilis
+    10
+    C.J. Anderson Den, RB
+    Team Harzich
+    Round 10
+    NO.
+    PLAYER
+    TEAM
+    1
+    DeSean Jackson TB, WR
+    Team Harzich
+    2
+    Broncos D/ST Den, D/ST
+    Paige Still Has Syphilis
+    3
+    Tevin Coleman Atl, RB
+    Kissing TDs
+    4
+    Martellus Bennett NE, TE
+    Josh Thordon
+    5
+    Cameron Meredith NO, WR
+    Team TradeRapist
+    6
+    DeVante Parker Mia, WR
+    Blowjobs Dont Take Virginity
+    7
+    Victor Cruz Chi, WR
+    CigsInside SMOKINJAYCUTLER
+    8
+    Cam Newton Car, QB
+    #FreeZeke Krispy Kareem
+    9
+    Adam Thielen Min, WR
+    Team Brannigan
+    10
+    Jordan Matthews NE, WR
+    Cyka Blyat
+    Round 11
+    NO.
+    PLAYER
+    TEAM
+    1
+    Duke Johnson Jr. Cle, RB
+    Cyka Blyat
+    2
+    Dak Prescott Dal, QB
+    Team Brannigan
+    3
+    James White NE, RB
+    #FreeZeke Krispy Kareem
+    4
+    Mohamed Sanu Atl, WR
+    CigsInside SMOKINJAYCUTLER
+    5
+    Chiefs D/ST KC, D/ST
+    Blowjobs Dont Take Virginity
+    6
+    Derrick Henry Ten, RB
+    Team TradeRapist
+    7
+    Mike Wallace Phi, WR
+    Josh Thordon
+    8
+    Tyrell Williams LAC, WR
+    Kissing TDs
+    9
+    Stephen Gostkowski NE, K
+    Paige Still Has Syphilis
+    10
+    Terrance West Bal, RB
+    Team Harzich
+    Round 12
+    NO.
+    PLAYER
+    TEAM
+    1
+    Kirk Cousins Min, QB
+    Team Harzich
+    2
+    Jamaal Charles Den, RB
+    Paige Still Has Syphilis
+    3
+    Rob Kelley Wsh, RB
+    Kissing TDs
+    4
+    Jason Witten Dal, TE
+    Josh Thordon
+    5
+    Kareem Hunt KC, RB
+    Team TradeRapist
+    6
+    Corey Coleman Cle, WR
+    Blowjobs Dont Take Virginity
+    7
+    Ben Roethlisberger Pit, QB
+    CigsInside SMOKINJAYCUTLER
+    8
+    Seahawks D/ST Sea, D/ST
+    #FreeZeke Krispy Kareem
+    9
+    Jonathan Stewart NYG, RB
+    Team Brannigan
+    10
+    Jacquizz Rodgers TB, RB
+    Cyka Blyat
+    Round 13
+    NO.
+    PLAYER
+    TEAM
+    1
+    Rishard Matthews Ten, WR
+    Cyka Blyat
+    2
+    John Brown Bal, WR
+    Team Brannigan
+    3
+    Justin Tucker Bal, K
+    #FreeZeke Krispy Kareem
+    4
+    Vikings D/ST Min, D/ST
+    CigsInside SMOKINJAYCUTLER
+    5
+    John Ross Cin, WR
+    Blowjobs Dont Take Virginity
+    6
+    Texans D/ST Hou, D/ST
+    Team TradeRapist
+    7
+    Randall Cobb GB, WR
+    Josh Thordon
+    8
+    Darren Sproles Phi, RB
+    Kissing TDs
+    9
+    Cameron Brate TB, TE
+    Paige Still Has Syphilis
+    10
+    Hunter Henry LAC, TE
+    Team Harzich
+    Round 14
+    NO.
+    PLAYER
+    TEAM
+    1
+    C.J. Prosise Sea, RB
+    Team Harzich
+    2
+    Giovani Bernard Cin, RB
+    Paige Still Has Syphilis
+    3
+    Corey Davis Ten, WR
+    Kissing TDs
+    4
+    Sterling Shepard NYG, WR
+    Josh Thordon
+    5
+    Jack Doyle Ind, TE
+    Team TradeRapist
+    6
+    Matt Bryant Atl, K
+    Blowjobs Dont Take Virginity
+    7
+    Jay Cutler Mia, QB
+    CigsInside SMOKINJAYCUTLER
+    8
+    Eric Ebron Ind, TE
+    #FreeZeke Krispy Kareem
+    9
+    Cardinals D/ST Ari, D/ST
+    Team Brannigan
+    10
+    Philip Rivers LAC, QB
+    Cyka Blyat
+    Round 15
+    NO.
+    PLAYER
+    TEAM
+    1
+    Giants D/ST NYG, D/ST
+    Cyka Blyat
+    2
+    Adam Vinatieri Ind, K
+    Team Brannigan
+    3
+    Jameis Winston TB, QB
+    #FreeZeke Krispy Kareem
+    4
+    Rex Burkhead NE, RB
+    CigsInside SMOKINJAYCUTLER
+    5
+    Marvin Jones Jr. Det, WR
+    Blowjobs Dont Take Virginity
+    6
+    Thomas Rawls NYJ, RB
+    Team TradeRapist
+    7
+    Dan Bailey Dal, K
+    Josh Thordon
+    8
+    Patriots D/ST NE, D/ST
+    Kissing TDs
+    9
+    Anquan Boldin Buf, WR
+    Paige Still Has Syphilis
+    10
+    Matt Prater Det, K
+    Team Harzich
+    Round 16
+    NO.
+    PLAYER
+    TEAM
+    1
+    Panthers D/ST Car, D/ST
+    Team Harzich
+    2
+    Josh Doctson Wsh, WR
+    Paige Still Has Syphilis
+    3
+    Dustin Hopkins Wsh, K
+    Kissing TDs
+    4
+    Jaguars D/ST Jax, D/ST
+    Josh Thordon
+    5
+    Wil Lutz NO, K
+    Team TradeRapist
+    6
+    Marcus Mariota Ten, QB
+    Blowjobs Dont Take Virginity
+    7
+    Mason Crosby GB, K
+    CigsInside SMOKINJAYCUTLER
+    8
+    Jesse James Pit, TE
+    #FreeZeke Krispy Kareem
+    9
+    Caleb Sturgis LAC, K
+    Team Brannigan
+    10
+    Chris Boswell Pit, K
+    Cyka Blyat"""
+
+    eight_split = text_2018.split('Round ')[1:]
+    for i in range(len(eight_split)):
+        for line in eight_split[i].split('\n'):
+            if ',' in line:
+                words = " ".join(line.split()[:-2])
+                print(f'"{words}": {i + 1},')
